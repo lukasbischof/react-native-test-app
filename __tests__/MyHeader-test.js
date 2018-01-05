8ebacb45c5f571a1305ea8f59a31e3fbade1b4ff
Lukas Bischof <lukas.bischof@renuo.ch>
2018-01-05 16:39:47 +0100

    Alter tests & App Center

--- __tests__/MyHeader-test.js
+++ __tests__/MyHeader-test.js
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import MyHeader from '../Components/MyHeader';
+import MyHeader from '../app/Components/MyHeader';
 import renderer from 'react-test-renderer';
 
 test('renders correctly', () => {
---
+++ ios/Pods/Target Support Files/Pods-ReactNativeTestApp-tvOSTests/Pods-ReactNativeTestApp-tvOSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReactNativeTestApp_tvOSTests : NSObject
+@end
+@implementation PodsDummy_Pods_ReactNativeTestApp_tvOSTests
+@end
