69cd800b59e9d55ac078fe17b4863b50338d206d
Lukas Bischof <lukas.bischof@renuo.ch>
2018-01-09 09:31:00 +0100

    remove everything

--- ios/Pods/Target Support Files/Pods-ReactNativeTestApp-tvOSTests/Pods-ReactNativeTestApp-tvOSTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactNativeTestApp_tvOSTests : NSObject
-@end
-@implementation PodsDummy_Pods_ReactNativeTestApp_tvOSTests
-@end
--- ios/Pods/Target Support Files/Pods-ReactNativeTestApp/Pods-ReactNativeTestApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ReactNativeTestApp : NSObject
-@end
-@implementation PodsDummy_Pods_ReactNativeTestApp
-@end
